3b4c0982b3f9e1cae8ecae6d7b925de7ed73224c
Sean Graham <seanmgrahamnj@yahoo.com>
2016-12-12 20:16:09 -0500

    added twilio controller and working send_text function

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,11 @@
+class TwilioController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def send_text
+    client = Twilio::REST::Client.new Rails.application.secrets.twilio_sid, Rails.application.secrets.twilio_token
+
+    message = client.messages.create from: Rails.application.secrets.twilio_number, to: '9739752575', body: 'Learning to send SMS you are.'
+
+    render plain: message.status
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'twilio/send_text' => 'twilio#send_text'
+
 end
