4bd1b559f4a6c46ac09f749c7380abdd30593fc9
Sean Graham <seanmgrahamnj@yahoo.com>
2016-12-23 16:19:26 -0500

    actionmailer config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,22 +25,17 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  config.action_mailer.raise_delivery_errors = true
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :domain         => 'mail.google.com',
+    :port           => 587,
+    :user_name      => 'hellowhisperscontact@gmail.com',
+    :password       => 'Remember1',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
